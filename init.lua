5c5f01834421bccc31a69ef3f1e17c74ddeb70af
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2025-03-23 15:03:56 -0400

    remove unused mini.icons

--- init.lua
+++ init.lua
@@ -962,7 +962,6 @@ require("lazy").setup({
 			-- - sr)'  - [S]urround [R]eplace [)] [']
 			require("mini.surround").setup()
 
-			require("mini.icons").setup()
 			-- Simple and easy statusline.
 			--  You could remove this setup call if you don't like it,
 			--  and try some other statusline plugin
