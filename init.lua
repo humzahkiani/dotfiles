dfeb9479df314805f57323af0ae939b6a79db1ed
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2025-04-12 14:56:29 -0400

    set colorscheme to rose-pine

--- init.lua
+++ init.lua
@@ -955,21 +955,34 @@ require("lazy").setup({
 		--   vim.cmd.colorscheme 'tokyonight-night'
 		-- end,
 	},
+	--
+	-- {
+	-- 	"projekt0n/github-nvim-theme",
+	-- 	name = "github-theme",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("github-theme").setup({
+	-- 			-- ...
+	-- 		})
+	--
+	-- 		vim.cmd("colorscheme github_dark_high_contrast")
+	-- 	end,
+	-- },
 
 	{
-		"projekt0n/github-nvim-theme",
-		name = "github-theme",
+		"rose-pine/neovim",
+		name = "rose-pine",
 		lazy = false,
-		priority = 1000,
 		config = function()
-			require("github-theme").setup({
-				-- ...
+			require("rose-pine").setup({
+				styles = {
+					italic = false,
+				},
 			})
-
-			vim.cmd("colorscheme github_dark_high_contrast")
+			vim.cmd("colorscheme rose-pine")
 		end,
 	},
-
 	-- Highlight todo, notes, etc in comments
 	{
 		"folke/todo-comments.nvim",
